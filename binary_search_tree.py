640832c3eced7cf325cb726a29394a0d376d00a0
amrit110 <amrit110@gmail.com>
2018-10-13 02:46:02 +0200

    Initial commit

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -11,7 +11,13 @@ are also handled.
 
 
 class BinarySearchTree:
-    """Binary Search Tree."""
+    """Binary Search Tree.
+
+    Attributes:
+        root (TreeNode): root node.
+        size (int): number of nodes in tree.
+
+    """
 
     def __init__(self):
         """Constructor."""
@@ -19,38 +25,86 @@ class BinarySearchTree:
         self.size = 0
 
     def length(self):
-        """Size of tree."""
+        """Size of tree.
+
+        Returns:
+            (int): number of nodes in tree.
+
+        """
         return self.size
 
     def __len__(self):
-        """Size of tree."""
+        """Size of tree.
+
+        Returns:
+            (int): number of nodes in tree.
+
+        """
         return self.size
 
     def __iter__(self):
-        """Iterate."""
+        """Iterate.
+
+        Returns:
+            (Iterator): iterator to go through nodes.
+
+        """
         return self.root.__iter__()
 
     def __setitem__(self, key, value):
-        """Add node."""
+        """Add node.
+
+        Args:
+            key (any): key of node.
+            value (any): value at node.
+
+        """
         self.put(key, value)
 
     def __getitem__(self, key):
-        """Get node."""
+        """Get node.
+
+        Args:
+            key (any): key of node.
+
+        Returns:
+            (any): value at node.
+
+        """
         return self.get(key)
 
     def __contains__(self, key):
-        """Overloads "in" method."""
+        """Overload "in" operator.
+
+        Args:
+            key (any): key of node.
+
+        Returns:
+            (bool): flag if node with key exists in tree.
+
+        """
         if self._get(key, self.root):
             return True
         else:
             return False
 
     def __delitem__(self, key):
-        """Call delete (del)."""
+        """Call delete (del).
+
+        Args:
+            key (any): key of node.
+
+        """
         self.delete(key)
 
     def put(self, key, val):
-        """Add node."""
+        """Add node.
+
+        Args:
+            key (any): key of node.
+            val (any): value at node.
+
+        """
         if self.root:
             self._put(key, val, self.root)
         else:
@@ -73,7 +127,15 @@ class BinarySearchTree:
                 current_node.right_child = TreeNode(key, val, parent=current_node)
 
     def get(self, key):
-        """Get node."""
+        """Get node.
+
+        Args:
+            key (any): key of node.
+
+        Returns:
+            (any): value at node.
+
+        """
         if self.root:
             res = self._get(key, self.root)
             if res:
@@ -94,7 +156,12 @@ class BinarySearchTree:
             return self._get(key, current_node.right_child)
 
     def delete(self, key):
-        """Delete node."""
+        """Delete node.
+
+        Args:
+            key (any): key of node.
+
+        """
         if self.size > 1:
             node_to_remove = self._get(key, self.root)
             if node_to_remove:
@@ -109,7 +176,12 @@ class BinarySearchTree:
             raise KeyError('Error, key {} not in tree'.format(key))
 
     def remove(self, current_node):
-        """Remove node from tree."""
+        """Remove node from tree.
+
+        Args:
+            current_node (TreeNode): node to remove.
+
+        """
         if current_node.is_leaf():
             if current_node == current_node.parent.left_child:
                 current_node.parent.left_child = None
@@ -148,10 +220,28 @@ class BinarySearchTree:
 
 
 class TreeNode:
-    """Node of binary search tree."""
+    """Node of binary search tree.
+
+    Attributes:
+        key (any): key of node.
+        payload (any): value at node.
+        left_child (TreeNode): possible left child node.
+        right_child (TreeNode): possible right child node.
+        parent (TreeNode): parent node.
+
+    """
 
     def __init__(self, key, val, left=None, right=None, parent=None):
-        """Constructor."""
+        """Constructor.
+
+        Args:
+            key (any): key of node.
+            val (any): value at node.
+            left (TreeNode): possible left child node.
+            right (TreeNode): possible right child node.
+            parent (TreeNode): parent node.
+
+        """
         self.key = key
         self.payload = val
         self.left_child = left
@@ -159,39 +249,84 @@ class TreeNode:
         self.parent = parent
 
     def has_left_child(self):
-        """Get left child of node."""
+        """Get left child of node.
+
+        Returns:
+            (TreeNode): possible left child node.
+
+        """
         return self.left_child
 
     def has_right_child(self):
-        """Get right child of node."""
+        """Get right child of node.
+
+        Returns:
+            (TreeNode): possible right child node.
+
+        """
         return self.right_child
 
     def is_left_child(self):
-        """Check if node is left child of parent node."""
+        """Check if node is left child of parent node.
+
+        Returns:
+            (bool): if node is left child of a parent node.
+
+        """
         return self.parent and self.parent.left_child == self
 
     def is_right_child(self):
-        """Check if node is left child of parent node."""
+        """Check if node is right child of parent node.
+
+        Returns:
+            (bool): if node is right child of a parent node.
+
+        """
         return self.parent and self.parent.right_child == self
 
     def is_root(self):
-        """Check if node is root node of tree."""
+        """Check if node is root node of tree.
+
+        Returns:
+            (bool): if node is root node of tree.
+
+        """
         return not self.parent
 
     def is_leaf(self):
-        """Check if node is leaf node (does not have child nodes)."""
+        """Check if node is leaf node (does not have child nodes).
+
+        Returns:
+            (bool): if node is leaf node.
+
+        """
         return not (self.right_child or self.left_child)
 
     def has_any_children(self):
-        """Check if node has any child nodes."""
+        """Check if node has any child nodes.
+
+        Returns:
+            (bool): if node has any child nodes.
+
+        """
         return self.right_child or self.left_child
 
     def has_both_children(self):
-        """Check if node has both right and left child nodes."""
+        """Check if node has both right and left child nodes.
+
+        Returns:
+            (bool): if node has both right and left child nodes.
+
+        """
         return self.right_child and self.left_child
 
     def __iter__(self):
-        """Iterator."""
+        """Iterator.
+
+        Yields:
+            keys in tree.
+
+        """
         if self:
             if self.has_left_child():
                 for elem in self.left_child:
@@ -202,7 +337,15 @@ class TreeNode:
                     yield elem
 
     def replace_node_data(self, key, value, lc, rc):
-        """Replace node with a new one."""
+        """Replace node with a new one.
+
+        Args:
+            key (any): key of node.
+            value (any): value at node.
+            lc (TreeNode): possible left child node.
+            rc (TreeNode): possible right child node.
+
+        """
         self.key = key
         self.payload = value
         self.left_child = lc
@@ -213,7 +356,12 @@ class TreeNode:
             self.right_child.parent = self
 
     def find_successor(self):
-        """Find successor node."""
+        """Find successor node.
+
+        Returns:
+            (TreeNode): successor node.
+
+        """
         succ = None
         if self.has_right_child():
             succ = self.right_child.find_min()
@@ -228,7 +376,12 @@ class TreeNode:
         return succ
 
     def find_min(self):
-        """Find the minimum key in a subtree."""
+        """Find the node with minimum key in a subtree.
+
+        Returns:
+            (TreeNode): node with min key.
+
+        """
         current = self
         while current.has_left_child():
             current = current.left_child
